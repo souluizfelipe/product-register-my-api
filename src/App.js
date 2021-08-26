3f6239b40033cb50628080b5578953b69012e7b5
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-26 14:56:15 -0300

    create Routes on app.js and create Pages folder with Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
-import './App.css';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <>
-      <h1>oi</h1>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
